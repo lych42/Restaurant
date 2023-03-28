797a314d58a9eda54ca2bc1a23a7b7bb7ae3e1d3
Charly Guerin <lannister135@gmail.com>
2023-03-28 13:18:07 +0200

    add entity image

---
+++ src/Entity/Image.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[Vich\Uploadable]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageName = null;
+
+    #[Vich\UploadableField(mapping: 'plat_images', fileNameProperty: 'imageName')]
+    private ?File $imageFile;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updateAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+
+	public function getImageFile(): ?File 
+                {
+            		return $this->imageFile;
+            	}
+	
+
+	public function setImageFile(?File $imageFile = null): self 
+                {
+            		$this->imageFile = $imageFile;
+            
+                    if ($this->imageFile instanceof UploadedFile) {
+                        $this->updateAt = new \DateTime('now');
+                    }
+            
+            		return $this;
+            	}
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+}
